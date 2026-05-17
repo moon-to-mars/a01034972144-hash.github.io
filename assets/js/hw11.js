8f05718806ca1638462a7bf4abcc0b07aaae4bf0
조선하 <a01034972144@gmail.com>
2026-05-17 15:13:38 +0000

    Q1

---
+++ assets/js/hw11.js
@@ -0,0 +1,15 @@
+// [숙제11] 동적 웹 페이지 구현
+// 2025-14294 조선하
+
+// Q1
+const themeBtn = document.querySelector("#theme-btn");
+const q1Box = document.querySelector("#q1-box");
+
+themeBtn.addEventListener("click", () => {
+  q1Box.classList.toggle("dark");
+  if (q1Box.classList.contains("dark")) {
+    themeBtn.textContent = "라이트 모드";
+  } else {
+    themeBtn.textContent = "다크 모드";
+  }
+});
